13c81e8a25db766a36bb9b214af019415a9968e1
equindar <2142808+Equindar@users.noreply.github.com>
2023-07-07 16:28:59 +0200

    fixed eslint errors

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -4,6 +4,7 @@ const Inventory = () => {
 	return (
 		<>
 			{Items.map(item => (
+				// eslint-disable-next-line react/jsx-key
 				<div>{item.name} - ID: {item.id}<br />Typ: {item.type}<br />Weight: {item.weight}</div>
 			))}
 		</>);
--- src/layouts/HintDevMode.js
+++ src/layouts/HintDevMode.js
@@ -1,23 +1,22 @@
-import { Avatar } from '@mui/material';
 import LogoDevMode from '../assets/svg/flasks-chemistry.svg';
 import classes from '../assets/HintDevMode.module.css';
 
 const HintDevMode = () => {
-    return (
-        <>
-            <section className={classes.hint}>
-                <h2>In Development</h2>
-                <div>
-                <img src={LogoDevMode} alt="SVG for Dev-Mode" />
-                    <p>
+	return (
+		<>
+			<section className={classes.hint}>
+				<h2>In Development</h2>
+				<div>
+					<img src={LogoDevMode} alt="SVG for Dev-Mode" />
+					<p>
                         This app is currently under development.<br />
                         All things are subject of change.
-                        <span>Feel free to look around and provide feedback, if you want.</span>
-                    </p>
-                </div>
-            </section>
-        </>
-    );
-}
+						<span>Feel free to look around and provide feedback, if you want.</span>
+					</p>
+				</div>
+			</section>
+		</>
+	);
+};
 
 export default HintDevMode;
