0cf61b36cc1633019ab277534a34c0364e0687af
equindar <2142808+Equindar@users.noreply.github.com>
2023-07-13 15:55:55 +0200

    clean up files

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders impressum link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Impressum/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,34 +1,33 @@
-import * as React from 'react';
 import './ProgressBar.css';
 
-const ProgressBar = (props) => {
-    return (
-        <div>
-            <div className="progress-bar">
-                <div className="progress-bar__inner">
-                    <div className="progress-bar__fill" style={{width: '70%'}}>
-                    </div>
-                </div>
-                <div className="progress-bar__label">Maro</div>
-            </div>
-            <div className="progress-bar">
-                <div className="progress-bar__inner">
-                    <div className="progress-bar__fill" style={{width: '5%'}}>
-                    </div>
-                </div>
-                <div className="progress-bar__label">Equindar</div>
-            </div>
-            <div className="progress-bar">
-                <div className="progress-bar__inner">
-                    <div className="progress-bar__fill" style={{width: '30%'}}>
-                    </div>
-                </div>
-                <div className="progress-bar__label">Mikko</div>
-            </div>
+const ProgressBar = () => {
+	return (
+		<div>
+			<div className="progress-bar">
+				<div className="progress-bar__inner">
+					<div className="progress-bar__fill" style={{width: '70%'}}>
+					</div>
+				</div>
+				<div className="progress-bar__label">Maro</div>
+			</div>
+			<div className="progress-bar">
+				<div className="progress-bar__inner">
+					<div className="progress-bar__fill" style={{width: '5%'}}>
+					</div>
+				</div>
+				<div className="progress-bar__label">Equindar</div>
+			</div>
+			<div className="progress-bar">
+				<div className="progress-bar__inner">
+					<div className="progress-bar__fill" style={{width: '30%'}}>
+					</div>
+				</div>
+				<div className="progress-bar__label">Mikko</div>
+			</div>
             
-    </div>
-    );
-}
+		</div>
+	);
+};
 
 
 export default ProgressBar;
