b5d66bd7dba6ed98595416bd3ac9a2159f29a22b
Equindar <equindar@gmail.com>
2023-08-01 23:12:23 +0200

    added character profile

---
+++ frontend/src/components/Character/CharacterProfile.js
@@ -0,0 +1,12 @@
+import { Card, CardHeader } from "@mui/material";
+
+const CharacterProfile = () => {
+    return (
+        <Card>
+            <CardHeader
+                title="Equindar" />
+        </Card>
+    );
+};
+
+export default CharacterProfile;
