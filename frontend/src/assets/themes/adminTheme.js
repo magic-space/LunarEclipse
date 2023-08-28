8639b12ab591ed1d2e591e0cd9711174b887b571
equindar <2142808+Equindar@users.noreply.github.com>
2023-08-28 16:25:58 +0200

    added additional themes

---
+++ frontend/src/assets/themes/adminTheme.js
@@ -0,0 +1,25 @@
+import { red, pink } from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles';
+
+export const adminTheme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: red,
+        secondary: pink,
+
+        background: {
+            dark: '#000000',
+            main: '#282C34',
+            light: '#5C6677',
+            paper: '#282C34',
+        },
+    },
+    breakpoints: {
+        values: {
+            mobile: 0,
+            tablet: 640,
+            laptop: 1024,
+            desktop: 1280,
+        },
+    },
+});
---
+++ frontend/src/assets/themes/devTheme.js
@@ -0,0 +1,25 @@
+import { orange, pink } from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles';
+
+export const devTheme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: orange,
+        secondary: pink,
+
+        background: {
+            dark: '#000000',
+            main: '#282C34',
+            light: '#5C6677',
+            paper: '#282C34',
+        },
+    },
+    breakpoints: {
+        values: {
+            mobile: 0,
+            tablet: 640,
+            laptop: 1024,
+            desktop: 1280,
+        },
+    },
+});
